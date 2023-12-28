fea27303a90c3afc0302b450d42c217838a4605c
Danilo Chaves <danchavesds@gmail.com>
2023-12-28 15:09:43 -0300

    Connection MYSQL DATABASE

---
+++ src/db/DB.java
@@ -0,0 +1,56 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        Properties properties = loadProperties();
+        String url = properties.getProperty("dburl");
+        if (connection == null){
+            try {
+                connection = DriverManager.getConnection(url,properties);
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    private static Properties loadProperties(){
+        try(FileInputStream fs = new FileInputStream("src/db.properties")){
+            Properties properties = new Properties();
+            properties.load(fs);
+            return properties;
+        }catch (IOException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeConnectino(){
+        try {
+            connection.close();
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement statement){
+        try {
+            statement.close();
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+    
+    public static void closeResultSet(ResultSet resultSet){
+        try {
+            resultSet.close();
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+}
